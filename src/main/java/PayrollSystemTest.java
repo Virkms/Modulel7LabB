b429a653f1667fcc43c6045b85b6a5f5e54ee736
sukhm <sukhm@192.168.1.160>
2023-04-30 20:47:01 -0400

    Module7LabB Payroll Updated

--- src/main/java/PayrollSystemTest.java
+++ src/main/java/PayrollSystemTest.java
@@ -14,11 +14,15 @@ public class PayrollSystemTest {
          ,new Date(04,16,2010));  
       CommissionEmployee commissionEmployee =                             
          new CommissionEmployee(                                          
-         "Sue", "Jones", "333-33-3333", 10000, .06, new Date (04,25,1999),new Date (03,01,2020));                      
+         "Sue", "Jones", "333-33-3333", 10000, .04, new Date (04,25,1999),new Date (03,01,2020));                      
       BasePlusCommissionEmployee basePlusCommissionEmployee =             
          new BasePlusCommissionEmployee(                                  
-         "Bob", "Lewis", "444-44-4444", 5000, .04, 300, new Date (02,14,1969), 
-                 new Date (05,15,2006));                  
+         "Bob", "Lewis", "444-44-4444", 5000, .02, 300, new Date (02,14,1969), 
+                 new Date (05,15,2006));       
+       basePlusCommissionEmployee =             
+         new BasePlusCommissionEmployee(                                  
+         "Manpreet", "Virk", "000-00-001", 2000, .02, 400, new Date (007,24,1968), 
+                 new Date (05,15,2006));      
 
       System.out.println("Employees processed individually:");
 
@@ -32,18 +36,19 @@ public class PayrollSystemTest {
          basePlusCommissionEmployee, 
          "earned", basePlusCommissionEmployee.earnings());
 
-      // create four-element Employee array
-      Employee[] employees = new Employee[4]; 
+     // create four-element Employee array
+      Employee[] employees = new Employee[5]; 
 
       // initialize array with Employees        
       employees[0] = salariedEmployee;          
       employees[1] = hourlyEmployee;            
       employees[2] = commissionEmployee;        
       employees[3] = basePlusCommissionEmployee;
+      employees[4] = basePlusCommissionEmployee;
 
       System.out.printf("Employees processed polymorphically:%n%n");
 
-      // generically process each element in array employees
+       //generically process each element in array employees
       for (Employee currentEmployee : employees) {
          System.out.println(currentEmployee); // invokes toString
 
@@ -66,19 +71,39 @@ public class PayrollSystemTest {
       } 
 
       // get type name of each object in employees array
-      for (int j = 0; j < employees.length; j++) {      
-         System.out.printf("Employee %d is a %s%n", j,  
-            employees[j].getClass().getName());  
+     //for (int j = 0; j < employees.length; j++) {      
+         //System.out.printf("Employee %d is a %s%n", j,  
+           // employees[j].getClass().getName());}  
+       
+           
+       
          
-      }  
-      int currentMonth = LocalDate.now().getMonthValue();
+       
+           
+System.out.println(" Question 10.13");
+// Manpreet Virk
+    //  int currentMonth = LocalDate.now().getMonthValue();
     for (Employee employee : employees) {
-      System.out.printf("%n%s%nearnings: $%,.2f%n", employee, employee.earnings());
-
-      if (employee.getBirthDate().getMonth() == currentMonth) {
-         System.out.println("Happy Birthday! You have received a $100 bonus!");
-         System.out.printf("New earnings: $%,.2f%n", employee.earnings() + 100);
-      }
-   }
-   }
+     // System.out.printf("%n%s%nearnings: $%,.2f%n", employee, employee.earnings());
+if (employee.earnings()  <= 600){
+    System.out.printf("%n%s%nearnings: $%,.2f%n", employee, employee.earnings());
+    System.out.println(employee.getFirstName() + " " + employee.getLastName() + " Is making less than 600");
 }
+else {
+    System.out.print("These Employees are making more than 600:" + " ");
+        System.out.println(employee.getFirstName() + " " + employee.getLastName() );
+
+}
+    /*if (employee.getBirthDate().getMonth() == currentMonth) {
+         System.out.println("Happy Birthday! You have received a $100 bonus!");
+         System.out.printf("New earnings: $%,.2f%n", employee.earnings() + 100);*/
+                 
+    
+      }
+      
+    }}
+   
+   
+
+
+   
